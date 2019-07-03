cc658ff7d12d0091a69772aa85d93152896de7af
linshuhe1 <695671725@qq.com>
2019-07-04 00:08:39 +0800

    draw a triangle

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,25 @@
 // settings
 const unsigned int SCR_WIDTH = 400;
 const unsigned int SCR_HEIGHT = 300;
+// 三角形的坐标
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+}; 
+// 顶点着色器 shader
+const char* vertexShaderSource = 
+"#version 330 core \n"
+"layout (location = 0) in vec3 aPos; \n"
+"void main(){ \n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);} \n";
+// 片段着色器 shader
+const char* fragmentShaderSource = 
+"#version 330 core \n"
+"out vec4 FragColor; \n"
+"void main(){ \n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}  \n";
+
 int main()
 {
     // glfw: initialize and configure
@@ -32,12 +51,49 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    // create VAO and VBO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.0f, 1.f, 0.0f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        // draw triangle
+        glBindVertexArray(VAO);
+        glUseProgram(shaderProgram);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
         // ---------------------------------------------------
         glfwSwapBuffers(window);
